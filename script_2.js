98f7b10f2d275e3a89454a96f53e3928b94913ec
yuvmironov <yuvmironov@gmail.com>
2017-11-15 14:28:46 +0300

    Добавлена функция изменения позиции цвета

--- script_2.js
+++ script_2.js
@@ -17,6 +17,12 @@ $(document).ready(function () {
         $('.gradient').css('background-image', property);
     }
 
+    function changePosition( id_element, positionForChange) {
+        var numberElement = id_element.replace('position_','');
+        position[numberElement] = positionForChange;
+        createGradient();
+    }
+
     function changeColor( id_element, colorForChange ) {
         var numberElement = id_element.replace('color_','');
         color[numberElement] = colorForChange;
@@ -34,22 +40,36 @@ $(document).ready(function () {
            var property = 'linear-gradient(to top, ' + color[0] + ' ' + position[0] +  '%, ';
            var string = "<label for='color_0' class='label'>Цвет 0</label>";
            string = string + '<input type="color" id="color_0" value="' + color[0] + '"/><br/>';
+           string = string + '<label for="position_0">Конечная позиция 0-го цвета</label><br/>';
+           string = string + '<input type="range" id="position_0" value="' + position[0] +'" min="0" max="100" step="1"><span class="gradient__position-text-0"> ' + position[0] +' </span><br/>';
            $('.control').append(string);
            while (i < item) {
                string ="";
                property = property + color[i] + ' ' + position[i] +'% ';
                string = '<label for="color_' + [i] + '" class="label">Цвет ' + [i] + '</label>';
                string = string + '<input type="color" id="color_' + [i] + '" value="'+ color[i] + '"/><br/>';
+               string = string + '<label for="position_' + i + '">Конечная позиция ' + i +'-го цвета</label><br/>';
+               string = string + '<input type="range" id="position_' + i + '" value="' + position[i] +'" min="0" max="100" step="1"><span class="gradient__position-text-' + i + '"> ' + position[i] +' </span><br/>';
                $('.control').append(string);
                i++;
            }
            property = property + ')';
            $('.gradient').css('background-image', property);
 
+           //меняем цвет
            $('.control').children('[type="color"]').on('change' ,function () {
                var id = $(this).attr('id');
                var color = $(this).val();
                changeColor(id, color);
+           });
+
+           //Меняем позицию цвета
+           $('.control').children('[type="range"]').on('change', function () {
+               var id = $(this).attr('id');
+               var pos = $(this).val();
+               $(this).next().html(pos);
+              changePosition(id, pos);
+
            });
 
            flag = true;
